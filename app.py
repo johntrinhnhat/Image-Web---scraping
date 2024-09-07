eb46074bc0a9db4d9e8b8605788e1b696aa2b571
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-08 00:04:09 +0700

    Update App

--- app.py
+++ app.py
@@ -34,12 +34,17 @@ def loading_images():
 def fetch_images(url):
     all_images = []
     
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    # Point the browser to the correct location
+    chrome_options.binary_location = "/usr/bin/chromium"
+
+    # Use chromedriver installed by the system package manager
+    driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=chrome_options)
+    
     driver.get(url)
 
     while True:
