fa942741201cd3f7adf652b887cb4cc582d6abf8
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-09-07 21:33:17 +0700

    Update App

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
 
 IMAGE_TYPES = ['.jpg', '.png', '.jpeg', '.jfif', '.webp']
 
